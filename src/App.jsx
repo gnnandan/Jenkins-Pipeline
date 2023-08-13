a2095f3cf56a5a271c283b522c4a1ad229384d80
Nandan G N <gnnandan7@gmail.com>
2023-08-13 20:46:36 +0530

    New Changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>Nandan G N</h1>
+        <h1>Curious Ecosystem</h1>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
